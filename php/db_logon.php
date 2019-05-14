a4e37a9f597d1e0af528f160c3dbe0bfb615cf82
unknown <pretender0906@gmail.com>
2019-05-14 15:12:49 +0200

    Test commit, added -f db_logon.php and a php directory

---
+++ php/db_logon.php
@@ -0,0 +1,4 @@
+//This file server to set local DB logon data, to prevent conflicts in usernames and such.
+//I'm not giving you my db info and don't want yours if it isn't "root", ""
+$username = "";
+$password = "";
