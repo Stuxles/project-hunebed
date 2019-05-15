642f57774293bbb5c5b234131818775f3f71a315
unknown <pretender0906@gmail.com>
2019-05-15 15:03:45 +0200

    Added functionality to get the browser engine

--- php/functions.php
+++ php/functions.php
@@ -6,7 +6,27 @@
     }
     
     function getBrowser() {
-            
+        $uaString = strtolower(getSafeUA());
+        $uaEngine = "undetermined";
+        $splitAt = "";
+        $uaVersion = "0.0.0";
+        if(strcmp($uaString, "No User-Agent given") == 0) {
+            return "Undetermined";
+        }
+        if(strpos($uaString, "webkit") !== FALSE) {
+            $uaEngine = "AppleWebKit / WebKit";
+            $splitAt = "webkit/";
+        } elseif(strpos($uaString, "gecko")) {
+            $uaEngine = "Gecko";
+            $splitAt = "gecko/";
+        } elseif(strpos($uaString, "blink")) {
+            $uaEngine = "Blink";
+            $splitAt = "webkit/";
+        }
+        if(strcmp($uaEngine, "undetermined") != 0) {
+            $uaVersion = explode(" ", explode($splitAt, $uaString)[1])[0];
+        }
+        return $uaEngine . " version " . $uaVersion;
     }
     
     function getDevice() {
@@ -27,8 +47,8 @@
                 if(strpos(strtolower($device), "mobile") || strpos(strtolower($device), "phone")) {
                     return "Unix-like Mobile OS";
                 }
-        } else if(strpos(strtolower($device), "android")) {
-            if(strpos(strtolower($device), "tv")) {
+        } elseif(strpos(strtolower($device), "android")) {
+            if(strpos(strtolower($device), "tv") || strpos(strtolower($device), "t.v.")) {
                 return "Android TV";
             }
         } else {
@@ -39,5 +59,11 @@
     function getSafeUA() {
         return $_SERVER['HTTP_USER_AGENT']??"No User-Agent given";
     }
-    printf(getSafeUA());
+    print_r(getSafeUA());
+    echo '<br />';
+    print_r(getDevice());
+    echo '<br />';
+    print_r(getBrowser());
+    echo '<br />';
+    
 ?>
