0cded7f1ad9a8b69c030e7a3cb07be6b57598ef5
unknown <pretender0906@gmail.com>
2019-05-15 14:25:26 +0200

    Gets the UA and User Device, as reported in the UA

--- php/functions.php
+++ php/functions.php
@@ -4,4 +4,40 @@
         $salt = str_pad(str_rot13($username), 22, "DCtNSdPbK9Ad9JNigUuKkK");
         return password_hash(str_pad($pass, 72, $salt), PASSWORD_BCRYPT, ["salt" => $salt]);
     }
+    
+    function getBrowser() {
+            
+    }
+    
+    function getDevice() {
+        $uaString = getSafeUA();
+        if(strcmp($uaString, "No User-Agent given") == 0) {
+            return "Undetermined";
+        }
+        $firstPos = strpos($uaString, "(")+1;
+        $len = strpos($uaString, ")") - $firstPos;
+        $device = explode(";", substr($uaString, $firstPos, $len))[0];
+        if(strpos(strtolower($device), "windows") !== FALSE) {
+            if(strpos($device, "NT") !== FALSE) {
+                return "Windows PC/Laptop";
+            } else {
+                return "Windows Phone/Tablet";
+            }
+        } elseif(strpos(strtolower($device), "x") !== FALSE) {
+                if(strpos(strtolower($device), "mobile") || strpos(strtolower($device), "phone")) {
+                    return "Unix-like Mobile OS";
+                }
+        } else if(strpos(strtolower($device), "android")) {
+            if(strpos(strtolower($device), "tv")) {
+                return "Android TV";
+            }
+        } else {
+            return $device;
+        }
+    }
+    
+    function getSafeUA() {
+        return $_SERVER['HTTP_USER_AGENT']??"No User-Agent given";
+    }
+    printf(getSafeUA());
 ?>
