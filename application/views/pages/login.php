a38c60f4b2de902b43f2c33fb92dd5e21e1efacd
Kevin Tabak <kevin_tabak@outlook.com>
2019-05-21 15:50:22 +0200

    Implemented codeignite framework
    Be sure you set the right base url in application/cofig/config.php

---
+++ application/views/pages/login.php
@@ -0,0 +1,36 @@
+<body>
+<div class="container">
+    <div class="row login">
+        <div class="col s12 m6 offset-m3">
+            <div class="card">
+                <div class="card-action light-blue white-text center-align">
+                    <h3>Login form</h3>
+                </div>
+                <div class="card-content">
+                    <div class="form-field">
+                        <label for="username">Username</label>
+                        <input type="text" id="username">
+                    </div>
+                    <br>
+                    <div class="form-field">
+                        <label for="password">Password</label>
+                        <input type="password" id="password">
+                    </div>
+                    <br>
+                    <div class="form-field">
+
+                        <label for="remember">
+                            <input type="checkbox" id="remember">
+                            <span>Remember me</span>
+                        </label>
+                    </div>
+                    <br>
+                    <div class="form-field center-align">
+                        <a href="<?=base_url()?>" class="btn-large light-blue">Login</a>
+                    </div>
+                    <br>
+                </div>
+            </div>
+        </div>
+    </div>
+
