be8e991b3d2de759492d7475be6937daa1ba253d
JiggleWiggle <games.j@live.nl>
2019-05-23 00:21:53 +0200

    Auto stash before merge of "master" and "origin/master"
    csv_write_reader

---
+++ php/csv_reader_writer.php
@@ -0,0 +1,21 @@
+<?php
+    //autoload from composer
+    require '../vendor/autoload.php';
+
+    use PhpOffice\PhpSpreadsheet\Spreadsheet;
+    use PhpOffice\PhpSpreadsheet\Writer\Csv;
+
+    $spreadsheet = new Spreadsheet();
+    $sheet = $spreadsheet->getActiveSheet();
+    $sheet->setCellValue('A1', 'Achternaam, Voornaam');
+    $sheet->setCellValue('B1','E-mail');
+    
+
+    $writer = new Csv($spreadsheet);
+    $writer->save('personeel.csv');
+    if($writer){
+        rename("personeel.csv", "../CSV_Files/personeel.csv");
+        echo "file created";
+    }
+    unset($spreadsheet);
+?>
