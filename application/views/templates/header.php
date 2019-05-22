c8d3325faed6e6a69908ac9546d014183a8b1a4c
Kevin Tabak <kevin_tabak@outlook.com>
2019-05-22 14:12:15 +0200

    Update header.php
    Fixed the link in the logo.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,22 +1,21 @@
 <body>
         <div class="nav">
-        <nav class="black nav-extended" role="none">
-            <div class="nav-wrapper container">
-            <a href="#" class="brand-logo"><i class="hb-ico">Hunico</i>logo</a>
-            <a href="#" data-target="side-menu" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-            <ul class="right hide-on-med-and-down">
-                <li class="waves-effect waves-dark"><a href="<?=base_url('login')?>">Logout</a></li>
-            </ul>
-            <div class="nav-content">
-                <ul class="tabs tabs-transparent">
-                <li class="tab"><a href="#">Home</a></li>
-                <li class="tab"><a href="#">Vragen</a></li>
-                <li class="tab"><a href="#">Pagina's</a></li>
-                </ul>
-            </div>  
-            <ul class="sidenav" id="side-menu">
-                <li><a href="#">Logout</a></li>
-            </ul>
-            </div>
-        </nav>
+            <nav class="black nav-extended">
+                <div class="nav-wrapper container">
+                    <a href="<?=base_url()?>" class="brand-logo"><i class="hb-ico" style="width:73px;"></i>logo</a>
+                    <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li class="waves-effect waves-dark"><a href="<?=base_url('login')?>">Logout</a></li>
+                    </ul>
+                </div>
+                <div class="nav-content container">
+                    <ul class="tabs tabs-transparent">
+                        <li class="tab"><a href="#">Home</a></li>
+                        <li class="tab"><a href="#">Vragen</a></li>
+                        <li class="tab"><a href="#">Pagina's</a></li>
+                    </ul>
+                </div>
+            </nav>
         </div>
+
+        
