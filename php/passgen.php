cae7d53193e6878b5f135ec86303b0afe865fa36
unknown <pretender0906@gmail.com>
2019-05-23 11:06:23 +0200

    Made a few tweaks to allow passgen to be used to either present the form or get a response string.

--- php/passgen.php
+++ php/passgen.php
@@ -5,30 +5,35 @@
  */
 function generateLink($email) {
 	$validDate = date("dmY-L");
-	return "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'] . "?email=" . base64_encode($validDate . "//" . $email);
+	return "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'] . "?reset=" . base64_encode($validDate . "//" . $email);
 }
 
-//Testcode, prints a proven working copy of the data
-if(!isset($_GET['email']) && !isset($_GET['passReset'])) {
+/**
+ *Parses $_GET to check if a valid password reset request was given
+ */
+function parseLink() {
+	if(!isset($_GET['reset']) && !isset($_GET['passReset'])) {
+		//TESTING CODE! THIS STILL NEEDS TO BE CHANGED TO REDIRECT TO HOME
 		$link = generateLink("pretender0906@gmail.com");
 		header("Location: " . $link, true, 303);
-} elseif(isset($_GET['passReset'])){
-	if(filter_var($_GET['passReset'], FILTER_VALIDATE_BOOLEAN) && isset($_GET['email'])) {
-		printf("Your password has been reset!");
+	} elseif(isset($_GET['passReset'])){
+		if(filter_var($_GET['passReset'], FILTER_VALIDATE_BOOLEAN) && isset($_GET['email'])) {
+			return "Your password has been reset!";
+		} else {
+			return "Something went wrong and your password did not change.";
+		}
 	} else {
-		printf("Something went wrong and your password did not change.");
-	}
-} else {
-	$info = explode("//", base64_decode($_GET['email']));
-	if(strcmp(date("dmY-L"), $info[0]) != 0) {
-		printf("This reset link is invalid or has expired. Please <a href='../index.php'>try logging in</a> or reset the password from there.");
-	} else {
-		//This is where basic HTML becomes relevant
-		echo "<!DOCTYPE html>";
-		echo "<html><head></head><body>";
-		echo "<p style='display: none;' id='hiddenMailP'>" . $info[1] . "</p>";
-		include("../html/newPassword.html");
-		echo "</body></html>";
+		$info = explode("//", base64_decode($_GET['reset']));
+		if(strcmp(date("dmY-L"), $info[0]) != 0) {
+			return "This reset link is invalid or has expired. Please <a href='../index.php'>try logging in</a> or reset the password from there.";
+		} else {
+			//This is where basic HTML becomes relevant
+			echo "<!DOCTYPE html><html><head></head><body><p style='display: none;' id='hiddenMailP'>" . $info[1] . "</p>";
+			include("../html/newPassword.html");
+			echo "</body></html>";
+			return "Please set a password you will remember.";
+		}
 	}
 }
+parseLink();
 ?>
