5a434e4b1810b1968664eb5d98d819f965ac0be3
Kevin Tabak <kevin_tabak@outlook.com>
2019-05-23 11:40:15 +0200

    Fixed routes

--- application/controllers/Moderator.php
+++ application/controllers/Moderator.php
@@ -1,12 +1,18 @@
 <?php
 class Moderator extends CI_Controller {
 
-    public function view()
+    public function view($page = 'moderator')
+    {
+        if ( ! file_exists(APPPATH.'views/moderator/'.$page.'.php'))
         {
-            $this->load->helper('url');
-            $this->load->view('templates/head');
-            $this->load->view('templates/header');
-            $this->load->view('moderator/moderator');
-            $this->load->view('templates/footer');
+            // Whoops, we don't have a page for that!
+            show_404();
         }
+
+        $this->load->helper('url');
+        $this->load->view('templates/head');
+        $this->load->view('templates/header');
+        $this->load->view('moderator/'.$page);
+        $this->load->view('templates/footer');
+    }
 }
--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,20 +1,20 @@
 <?php
 class Pages extends CI_Controller {
 
-        public function view($page = 'home')
+    public function view($page = 'home')
+    {
+        if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
         {
-            if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
-            {
-                // Whoops, we don't have a page for that!
-                show_404();
-            }
-
-            $data['title'] = ucfirst($page); // Capitalize the first letter
-
-            $this->load->helper('url');
-            $this->load->view('templates/head', $data);
-            $this->load->view('templates/header', $data);
-            $this->load->view('pages/'.$page, $data);
-            $this->load->view('templates/footer', $data);
+            // Whoops, we don't have a page for that!
+            show_404();
         }
+
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+
+        $this->load->helper('url');
+        $this->load->view('templates/head', $data);
+        $this->load->view('templates/header', $data);
+        $this->load->view('pages/'.$page, $data);
+        $this->load->view('templates/footer', $data);
+    }
 }
