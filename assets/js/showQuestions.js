c9bfe6cf3d6bfdf64040d13a586fd9639eed4ff4
JiggleWiggle <games.j@live.nl>
2019-05-29 14:53:34 +0200

    added baseurl in JS
    added base url in JS and shown questions

--- assets/js/showQuestions.js
+++ assets/js/showQuestions.js
@@ -1,11 +1,14 @@
 
 function create_il(){
+
 var x = 0;
+var pathArray = window.location.pathname.split( '/' );
+
 db.collection('Questions').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
         var ul = document.getElementById('qList');
         var li = document.createElement('LI');
-        li.className = "collection-item avat0ar" ;
+        li.className = "collection-item avatar";
         var span = document.createElement('SPAN');
         span.classname = "title";
         span.textContent = doc.data().Question;
@@ -14,14 +17,19 @@ db.collection('Questions').get().then((snapshot) => {
         var persoonP = document.createElement('P');
         var questionP = document.createElement('P');
         questionP.textContent = doc.data().Question_answer;
-
-
+        var a = document.createElement('A');
+        a.href = pathArray[3].replace("moderator", "").concat("addQuestion");
+        a.textContent = ">>"
         li.appendChild(b);
         li.appendChild(persoonP);
         li.appendChild(questionP);
+        li.appendChild(a);
         ul.appendChild(li);
     
     })
 })
 }
 create_il()
+
+
+
