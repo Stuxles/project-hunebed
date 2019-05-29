41af5b8116059a83e91aedd47ab55b9fc93e6639
Soner Mulder <sonermulder@gmail.com>
2019-05-29 12:56:49 +0200

    Fixes

---
+++ application/views/pages/log-out.php
@@ -0,0 +1,20 @@
+<div class="container content">
+
+    <div class="container"
+        <div class="col s12 m6 offset-m3">
+            <div class="card">
+                <div class="card-action light-blue white-text center-align">
+                    <h3>Je bent uitgelogt</h3>
+                </div>
+                <div class="card-content blue-grey-text">
+                    <br>
+                    <p>Je wordt over 5 seconden doorverwezen naar de hoofdpagina.</p>
+                    <br>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
