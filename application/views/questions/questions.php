1fd494e26851fad0080e635d4fe7018360426325
Kevin Tabak <kevin_tabak@outlook.com>
2019-05-30 19:33:50 +0200

    Removed unnecessary html and php

--- application/views/questions/questions.php
+++ application/views/questions/questions.php
@@ -6,24 +6,11 @@
                 <input class="center" id="search-question" placeholder="Zoeken" id="first_name" type="text">
             </div>
         </div>
-        <form id="testForm" class="center">
-            <button class="btn testButton">Show Questions</button>
-        </form>
         <div class="container">
             <ul class="pagination center question-pagination">
             </ul>
             <div class="questionListContainer">
             </div>
-            <!-- <?php for ($x = 0; $x <= 0; $x++) { ?>
-            <div class="card question-card hoverable">
-                <div class="card-content">
-                    <p>I am a very simple card. I am good at containing small bits of information?</p>
-                </div>
-                <div class="card-action">
-                    <a class="hb-yellow-text" href="<?=base_url('questions/show')?>">Lees meer</a>
-                </div>
-            </div>
-            <?php } ?> -->
         </div>
     </div>
     <br><br>
