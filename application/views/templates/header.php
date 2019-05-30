5bd8730aa6a0abba517cc7ec1fc25a06affad649
Kevin Tabak <kevin_tabak@outlook.com>
2019-05-30 17:15:52 +0200

    Revert "Log out page"

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,20 +2,20 @@
         <div class="nav">
         <nav class="black nav-extended" role="none">
             <div class="nav-wrapper container">
-            <a href="#" class="brand-logo"><i class="hb-ico">Hunico</i>Hunebedcentrum</a>
+            <a href="#" class="brand-logo"><i class="hb-ico">Hunico</i>logo</a>
             <a href="#" data-target="side-menu" class="sidenav-trigger"><i class="material-icons">menu</i></a>
             <ul class="right hide-on-med-and-down">
-                <li class="waves-effect waves-dark"><a href="<?=base_url('log-out')?>">Logout</a></li>
+                <li class="waves-effect waves-dark"><a href="<?=base_url('login')?>">Logout</a></li>
             </ul>
             <div class="nav-content">
                 <ul class="tabs tabs-transparent">
-                <li class="tab"><a href="<?=base_url('home')?>">Home</a></li>
-                <li class="tab"><a href="<?=base_url('#')?>">Vragen</a></li>
-                <li class="tab"><a href="<?=base_url('Pages')?>">Pagina's</a></li>
+                <li class="tab"><a href="#">Home</a></li>
+                <li class="tab"><a href="#">Vragen</a></li>
+                <li class="tab"><a href="#">Pagina's</a></li>
                 </ul>
             </div>  
             <ul class="sidenav" id="side-menu">
-                <li><a href="log-out">Logout</a></li>
+                <li><a href="#">Logout</a></li>
             </ul>
             </div>
         </nav>
