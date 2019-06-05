0abd4a96ed5c35470fc98dbd33d85f30252a82d7
Kevin Tabak <kevin_tabak@outlook.com>
2019-06-05 11:26:53 +0200

    Added fontawesome

--- application/views/templates/head.php
+++ application/views/templates/head.php
@@ -11,5 +11,6 @@
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link href="<?=base_url('assets/css/materialize.css')?>" type="text/css" rel="stylesheet" media="screen,projection"/>
         <link href="<?=base_url('assets/css/style.css'); ?>" rel="stylesheet" type="text/css" >
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
         <link rel='shortcut icon' type='image/x-icon' href='<?=base_url('assets/img/favicon.png'); ?>'/>
     </head>
