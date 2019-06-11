c8b6fa611dd80430fb86e81d741f79377082d0b5
unknown <pretender0906@gmail.com>
2019-06-11 11:52:04 +0200

    added update function for editing a user

--- assets/js/editUser.js
+++ assets/js/editUser.js
@@ -7,3 +7,48 @@ document.getElementById("textChanger2").onclick = function() {
 document.getElementById("textChanger3").onclick = function() {
     document.getElementById("firstdiv3").innerHTML = "<div class='input-field col s12'><i class='material-icons prefix'>email</i><input id='email' type='email' class='validate'><label for='email'>Email</label></div>";
 }
+
+//firebase.Auth.currentUser.uid
+//document.getElementById("push2db").onclick = (
+function updateUser() {
+	let userRef = db.collection("Users").doc("BGjfbunGEYMuaBa1VxPdUMLiGjF2");
+	userRef.get().then(function(doc){
+		alert("hodor");
+		let user = { };
+		var userRoles = {
+			Algemeen: "/Roles/dIQfWKaJSNZ9raCPu1QN",
+			Horeca: "/Roles/HHLpbBJXp9Od7ExLHBKD",
+			Museum: "/Roles/JAvkRbnS1GzZcLGAa7P5",
+			Winkel: "/Roles/vt2CPNKWcAhWMHVVq5iw"
+		};
+		if(doc.exists){
+			alert("exists");
+		if(document.getElementById("first_name") != null) { //this was processed from moderator pages
+			user.FirstName = document.getElementById("first_name");
+			user.LastName = document.getElementById("last_name");
+		}
+		if(document.getElementById("email") != null){
+			user.Email = document.getElementById("email");
+		}
+		let activeRoles = [];
+		activeRoles.push(db.doc(userRoles.Algemeen));
+		if(document.getElementById("horecaRole").value === true) {
+			activeRoles.push(db.doc(userRoles.Horeca));
+		}
+
+		if(document.getElementById("museumRole").value === true) {
+			activeRoles.push(db.doc(userRoles.Museum));
+		}
+
+		if(document.getElementById("winkelRole").value === true) {
+			activeRoles.push(db.doc(userRoles.Winkel));
+		}
+		alert(Object.entries(user));
+		user.Roles = activeRoles;
+		userRef.update(user);
+		}
+		else {
+			alert("User ain't real");
+		}
+	});
+}
