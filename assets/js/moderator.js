607004352af2b90d06766bbdeda88f1d3a380475
Sander111 <sanderpaping@live.nl>
2019-06-11 12:31:59 +0200

    Added connection to firestore to get user and put then all in a table
    Functie/categorie does not yet work

--- assets/js/moderator.js
+++ assets/js/moderator.js
@@ -6,7 +6,7 @@ function renderUser(doc) {
     let tr = document.createElement('tr');
     let FirstName = document.createElement('td');
     let LastName = document.createElement('td');
-    let email = document.createElement('td');
+    let Email = document.createElement('td');
     let Roles = document.createElement('td');
     let Edit = document.createElement('td');
     let Button = document.createElement('a');
@@ -15,15 +15,15 @@ function renderUser(doc) {
     tr.setAttribute('data-id', doc.id);
     FirstName.textContent = doc.data().FirstName;
     LastName.textContent = doc.data().LastName;
-    email.textContent = doc.data().email;
-    Roles.textContent = doc.data().Roles;
+    Email.textContent = doc.data().Email;
+    Roles.textContent = doc.data().Roles.join(" - ");
     Button.className = 'waves-effect waves-light hb-red-bg btn-floating';
     Icon.className = 'material-icons left'; 
     Icon.textContent = 'edit';
 
     tr.appendChild(FirstName);
     tr.appendChild(LastName);
-    tr.appendChild(email);
+    tr.appendChild(Email);
     tr.appendChild(Roles);
     tr.appendChild(Edit);
     Edit.appendChild(Button);
@@ -31,11 +31,11 @@ function renderUser(doc) {
 
     userList.appendChild(tr);
 
-    // deleting data
-    // Edit.addEventListener('click', (e) => {
+    // redirecting to edit user page
+    // Button.addEventListener('click', (e) => {
     //     e.stopPropagation();
     //     let id = e.target.parentElement.getAttribute('data-id');
-    //     db.collection('Users').doc(id).delete();
+    //     
     // });
 }
 
@@ -43,7 +43,7 @@ function renderUser(doc) {
 db.collection('Users').get().then(snapshot => {
     snapshot.docs.forEach(doc => {
         renderUser(doc);
-        //console.log(doc.data())
+        console.log(doc.data())
     });
 });
 
