88ed78574224ba0d8350681e2da23fed9bb07002
unknown <pretender0906@gmail.com>
2019-06-13 10:40:24 +0200

    Preparation for merging Kevin's code and rebuilding useredit on that

--- assets/js/editUser.js
+++ assets/js/editUser.js
@@ -8,40 +8,26 @@ document.getElementById("textChanger3").onclick = function() {
     document.getElementById("firstdiv3").innerHTML = "<div class='input-field col s12'><i class='material-icons prefix'>email</i><input id='email' type='email' class='validate'><label for='email'>Email</label></div>";
 }
 
+let userRoles = {}; //fill this with the code from Kevin
+let activeRoles = []; //fill this with Kevin's code as well
+
 function updateUser() {
 	let userRef = db.collection("Users").doc(firebase.Auth.currentUser.uid.toString());
 	userRef.get().then(function(doc){
 		let user = { };
 		if(doc.exists){
-		if(document.getElementById("first_name") != null) { //this was processed from moderator pages
-			user.FirstName = document.getElementById("first_name");
-			user.LastName = document.getElementById("last_name");
-		}
-		if(document.getElementById("email") != null){
-			user.Email = document.getElementById("email");
-		}
-		let activeRoles = {
-			Algemeen: db.doc(userRoles.Algemeen),
-			Horeca: null,
-			Museum: null,
-			Winkel: null
-			};
-		if(document.getElementById("horecaRole").value === true) {
-			activeRoles.Horeca = db.doc(userRoles.Horeca);
-		}
+			if(document.getElementById("first_name") != null) { //this was processed from moderator pages
+				user.FirstName = document.getElementById("first_name");
+				user.LastName = document.getElementById("last_name");
+			}
+			if(document.getElementById("email") != null){
+				user.Email = document.getElementById("email");
+			}
+			for(let role of userRoles) {
 
-		if(document.getElementById("museumRole").value === true) {
-			activeRoles.Museum = db.doc(userRoles.Museum);
-		}
-
-		if(document.getElementById("winkelRole").value === true) {
-			activeRoles.Winkel = db.doc(userRoles.Winkel);
-		}
-		user.Roles = activeRoles;
-		userRef.update(user);
-		}
-		else {
-			alert("User ain't real");
+			}
+			user.Roles = activeRoles;
+			userRef.update(user);
 		}
 	});
 }
