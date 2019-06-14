49e5027d437a526e02c99467da6cc1bcc7b2b289
Sander111 <sanderpaping@live.nl>
2019-06-14 12:09:17 +0200

    Quiz temporary generate question added

---
+++ application/views/quiz/quiz.php
@@ -0,0 +1,98 @@
+<div class="container content">
+    <div class="section">
+        <div class="row">
+            <div class="col s12 center">
+                <h3>Quiz</h3>
+            </div>
+        </div>
+        <div class="divider"></div>
+        <div class="row">
+            <div class="col s12 center">
+                <h5>Kies de categorie waarover je een quiz wilt maken.</h5>
+            </div>
+        </div>
+        </section>
+        <div class="row">
+            <div class="col s12 m6">
+            <a href="<?= base_url('quiz/quizQuestion') ?>" class="white-text">
+                <div class="card medium hoverable hb-blue">
+                    
+                    <div class="card-content center hb-blue">
+                        <span class="card-title white-text">Algemeen</span>
+                    </div>
+                    <div class="card-image">
+                        <img src="<?= base_url('assets/img/algemeen.jpg') ?>">
+                    </div>
+                    <div class="card-action right-align hb-blue">
+                    <div class="hb-red-bg btn">Doe de quiz!</div>
+                    </div>
+                </div>
+                </a>
+            </div>
+            <div class="col s12 m6">
+            <a href="<?= base_url('quiz/quizQuestion') ?>" class="white-text">
+                <div class="card medium hoverable hb-blue">
+                    
+                    <div class="card-content center hb-blue">
+                        <span class="card-title white-text">Horeca</span>
+                    </div>
+                    <div class="card-image">
+                        <img src="<?= base_url('assets/img/horeca.jpg') ?>">
+                    </div>
+                    <div class="card-action right-align hb-blue">
+                    <div class="hb-red-bg btn">Doe de quiz!</div>
+                    </div>
+                </div>
+                </a>
+            </div>
+            <div class="col s12 m6">
+            <a href="<?= base_url('quiz/quizQuestion') ?>" class="white-text">
+                <div class="card medium hoverable hb-blue">
+                    
+                    <div class="card-content center hb-blue">
+                        <span class="card-title white-text">Museumwinkel</span>
+                    </div>
+                    <div class="card-image">
+                        <img src="<?= base_url('assets/img/museumwinkel.jpg') ?>">
+                    </div>
+                    <div class="card-action right-align hb-blue">
+                    <div class="hb-red-bg btn">Doe de quiz!</div>
+                    </div>
+                </div>
+                </a>
+            </div>
+            <div class="col s12 m6">
+            <a href="<?= base_url('quiz/quizQuestion') ?>" class="white-text">
+                <div class="card medium hoverable hb-blue">
+                    
+                    <div class="card-content center hb-blue">
+                        <span class="card-title white-text">Museum</span>
+                    </div>
+                    <div class="card-image">
+                        <img src="<?= base_url('assets/img/museum.jpg') ?>">
+                    </div>
+                    <div class="card-action right-align hb-blue">
+                        <div class="hb-red-bg btn">Doe de quiz!</div>
+                    </div>
+                </div>
+                </a>
+            </div>
+            <div class="col s12 m6">
+            <a href="<?= base_url('quiz/quizQuestion') ?>" class="white-text">
+                <div class="card medium hoverable hb-blue">
+                    
+                    <div class="card-content center hb-blue">
+                        <span class="card-title white-text">Oertijdpark</span>
+                    </div>
+                    <div class="card-image">
+                        <img src="<?= base_url('assets/img/oertijdpark.jpg') ?>">
+                    </div>
+                    <div class="card-action right-align hb-blue">
+                        <div class="hb-red-bg btn">Doe de quiz!</div>
+                    </div>
+                </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
