4ac4722d02b10d18a2018851ab4936cc1cb23331
Kevin Tabak <kevin_tabak@outlook.com>
2019-06-14 13:19:07 +0200

    Fix Login/Logout button

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,8 +7,8 @@
                 <a href="<?=base_url()?>" class="brand-logo"><i class="hb-ico" style="width:73px;"></i>Hunebedcentrum</a>
                 <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li class="waves-effect waves-dark logged-in" style="display:none;" ><a class="waves-effect waves-light btn hb-red-bg" href="<?=base_url('log-out')?>"></i>Logout</a></li>
-                    <li class="waves-effect waves-dark logged-out" style="display:none;" ><a class="waves-effect waves-light btn hb-red-bg" href="<?=base_url('login')?>">Login</a></li>
+                    <li class="logged-in" style="display:none;" ><a class="waves-effect waves-light btn hb-red-bg" href="<?=base_url('log-out')?>"></i>Logout</a></li>
+                    <li class="logged-out" style="display:none;" ><a class="waves-effect waves-light btn hb-red-bg" href="<?=base_url('login')?>">Login</a></li>
                 </ul>
             </div>
             <div class="nav-content container">
@@ -19,8 +19,5 @@
                     <li class="tab admin" style="display:none"><a href="<?=base_url('moderator')?>">Moderatie</a></li>
                 </ul>
             </div>
-            <ul class="sidenav" id="side-menu">
-                <li><a href="log-out">Logout</a></li>
-            </ul>
         </nav>
     </div>
