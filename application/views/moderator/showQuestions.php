ab85ec59a2b3ca5ae4fc0157556aff048e0aa4ae
JiggleWiggle <games.j@live.nl>
2019-06-17 14:42:58 +0200

    added 2 list in ingezonden
    added 2 list in ingezonden

--- application/views/moderator/showQuestions.php
+++ application/views/moderator/showQuestions.php
@@ -1,25 +1,25 @@
 <div class="container content">
-
-    <div class="section">
-        <div class="row">
-            <div class="col s12 center">
-                <h3>Ingezonden vragen</h3>
-            </div>
-        </div>
-        <div class="container">
-            <ul class="collection" id = 'qList'>
-
-            </ul>
-            <div class="divider"></div>
-        </div>
-    </div>
-
-    <div class="section no-pad-bot">
-        <div class="container">
-            <div class="row center">
-                <a href="<?= base_url('moderator/moderator') ?>" class=" btn waves-effect orange waves-light"><i class="material-icons left">arrow_back</i>Terug</a>
-
-            </div>
-            <div class="divider"></div>
-        </div>
-    </div>
+<div class="section">
+   <div class="row">
+      <div class="col s12 center">
+         <h3>Ingezonden vragen</h3>
+      </div>
+   </div>
+   <div class="container">
+      <h4>Gebruiker gestelde vragen</h4>
+      <ul class="collection" id = 'qListSubmit'>
+      </ul>
+      <h4>Goedgekeurde vragen</h4>
+      <ul class="collection" id = 'qListApproved'>
+      </ul>
+      <div class="divider"></div>
+   </div>
+</div>
+<div class="section no-pad-bot">
+   <div class="container">
+      <div class="row center">
+         <a href="<?= base_url('moderator/moderator') ?>" class=" btn waves-effect orange waves-light"><i class="material-icons left">arrow_back</i>Terug</a>
+      </div>
+      <div class="divider"></div>
+   </div>
+</div>
--- assets/js/showQuestions.js
+++ assets/js/showQuestions.js
@@ -1,5 +1,51 @@
+
+function submittedQuestions(){
+    db.collection('Submitted_Questions').get().then((snapshot) => {
+        snapshot.docs.forEach(doc => {
+            var ul = document.getElementById('qListSubmit');
+            var li = document.createElement('LI');
+            var li = document.createElement('LI');
+            li.className = "collection-item avatar";
+            var span = document.createElement('SPAN');
+            span.classname = "title";
+            span.textContent = doc.data().Question;
+            var b = document.createElement('B');
+            b.appendChild(span);
+            var persoonP = document.createElement('P');
+            var questionP = document.createElement('P');
+            questionP.textContent = doc.data().Question_answer;
+            var a = document.createElement('A');
+
+            //create base url for delete/approve Question
+            var butt_Delete_Approve = document.createElement("BUTTON");
+            butt_Delete_Approve.className = "waves-effect waves-light red btn";
+            butt_Delete_Approve.textContent = "Goedkeuring / verwijderen";
+            var aDelApr = document.createElement('A');
+            aDelApr.setAttribute('delete-id', doc.id);
+            aDelApr.appendChild(butt_Delete_Approve);
+            butt_Delete_Approve.addEventListener('click', (e) => {
+                var id = e.target.parentElement.getAttribute('delete-id');
+                window.sessionStorage.setItem('delete-id', id);
+                location.href = pathArray[3].replace("moderator", "").concat("removeApproveQuestion");
+            })
+            
+            //append button to <A> tag
+            aDelApr.appendChild(butt_Delete_Approve);
+            li.appendChild(aDelApr);
+            //appends the rest of the elements to the UL Tag
+            li.appendChild(b);
+            li.appendChild(persoonP);
+            li.appendChild(questionP); 
+            li.appendChild(a);
+            ul.appendChild(li);
+        })
+    })
+}
+
+
+
 //create the IL tags and appends to the existing UL tag
-function create_il(){
+function approvedQuestions(){
 
     // required to find the base url
     var pathArray = window.location.pathname.split( '/' );
@@ -7,7 +53,7 @@ function create_il(){
     db.collection('Questions').get().then((snapshot) => {
         snapshot.docs.forEach(doc => {
             //creates LI elements which partiallu displays the question contents
-            var ul = document.getElementById('qList');
+            var ul = document.getElementById('qListApproved');
             var li = document.createElement('LI');
             li.className = "collection-item avatar";
             var span = document.createElement('SPAN');
@@ -59,5 +105,5 @@ function create_il(){
         })
     })
 }
-//call function create_il()
-create_il();
+//call function approvedQuestions()
+approvedQuestions();
