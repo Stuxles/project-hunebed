dbc32995c981caf989948b26f3500e9133f2b696
JiggleWiggle <games.j@live.nl>
2019-06-18 11:27:04 +0200

    very minor changes

--- assets/js/approveDelete.js
+++ assets/js/approveDelete.js
@@ -4,24 +4,23 @@ var ref = db.collection('Questions').doc(dataID);
 console.log(dataID);
 function question(){
     ref.get().then((doc) => {
-        console.log(doc.data().Question);
+        console.log(doc);
         //checks if docs exits
         if (doc.exists) {
             //creates the variables that refer to the id's of the divs
             var text = document.getElementById('textarea1');
-            for(x=2; x <= 7;x++){
-            window["text" + x] = document.getElementById('textarea'.concat(x));
-            }
+            // for(x=2; x <= 7;x++){
+            // window["text" + x] = document.getElementById('textarea'.concat(x));
+            // }
 
             //sets firestore data into input field values
-            text.textContent = doc.data().Question;
+            text.value = doc.data().Question;
 
             //when back button press clear sessionstorage
             document.getElementById("terug").addEventListener('click', (e) => {
                 clearSessionStorage();
             });
 
-            
         } else {
                 console.log("No such document!");
         }
