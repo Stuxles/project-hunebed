05436af40ac74c675786f6f6589a134fbafdb03f
Sander111 <sanderpaping@live.nl>
2019-06-19 15:34:10 +0200

    table fixed

--- application/views/moderator/showusers.php
+++ application/views/moderator/showusers.php
@@ -8,35 +8,15 @@
             <table class="highlight responsive-table">
                 <thead>
                 <tr>
-                    <th>Naam</th>
+                    <th>Voornaam</th>
+                    <th>Achternaam</th>
                     <th>E-mailadres</th>
-                    <th>Functie</th>
                     <th>Aanpassen</th>
                 </tr>
                 </thead>
 
                 <tbody id="usertable">
-                <tr>
-                            <td>Harrie</td>
-                            <td>Wolters</td>
-                            <td>hwolters@hunebedcentrum.nl</td>
-                            <td>Algemeen Directeur</td>
-                            <td><a href="<?= base_url('moderator/editUser') ?>" class="waves-effect waves-light hb-red-bg btn-floating"><i class="material-icons left">edit</i></a></td>
-                        </tr>
-                        <tr>
-                            <td>Gijs</td>
-                            <td>Klompmaker</td>
-                            <td>gklompmaker@hunebedcentrum.nl</td>
-                            <td>Marketing</td>
-                            <td><a href="<?= base_url('moderator/editUser') ?>" class="waves-effect waves-light hb-red-bg btn-floating"><i class="material-icons left">edit</i></a></td>
-                        </tr>
-                        <tr>
-                            <td>Jopie</td>
-                            <td>Jakkerman</td>
-                            <td>Jjakkerman@hunebedcentrum.nl</td>
-                            <td>Winkel</td>
-                            <td><a href="<?= base_url('moderator/editUser') ?>" class="waves-effect waves-light hb-red-bg btn-floating"><i class="material-icons left">edit</i></a></td>
-                        </tr>
+
                 </tbody>
             </table>
             <div class="divider"></div>
