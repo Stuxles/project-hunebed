02507932120dd89307d1fcccfd3c77c4119d457f
Sander111 <sanderpaping@live.nl>
2019-06-19 15:16:45 +0200

    Update showUsers

--- application/views/moderator/showusers.php
+++ application/views/moderator/showusers.php
@@ -15,105 +15,11 @@
                 </tr>
                 </thead>
 
-                <tbody>
-                <tr>
-                    <td>Harrie Wolters</td>
-                    <td>hwolters@hunebedcentrum.nl</td>
-                    <td>Algemeen Directeur</td>
-                    <td><a class="waves-effect waves-light orange btn-floating"><i class="material-icons left">edit</i></a></td>
-                </tr>
-                <tr>
-                    <td>Gijs Klompmaker</td>
-                    <td>gklompmaker@hunebedcentrum.nl</td>
-                    <td>Marketing</td>
-                    <td><a class="waves-effect waves-light orange btn-floating"><i class="material-icons left">edit</i></a></td>
-                </tr>
-                <tr>
-                    <td>Jopie Jakkerman</td>
-                    <td>Jjakkerman@hunebedcentrum.nl</td>
-                    <td>Winkel</td>
-                    <td><a class="waves-effect waves-light orange btn-floating"><i class="material-icons left">edit</i></a></td>
-                </tr>
+                <tbody id="usertable">
                 </tbody>
             </table>
             <div class="divider"></div>
         </div>
     </div>
-
-    <div class="section no-pad-bot">
-        <div class="container">
-            <h4>Statestieken $naam</h4>
-            <div class="row">
-                <div class="col s12 m6">
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="../../../assets/img/plattegrond.jpg">
-                            <span class="card-title black-text">$naam</span>
-                            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">edit</i></a>
-                        </div>
-                        <div class="card-content">
-                            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="divider"></div>
-            <div class="row">
-                <div class="col s12">
-                    <!-- grafiek -->
-                </div>
-            </div>
-            <div class="row">
-                <div class="col s12 m6">
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="../../../assets/img/favicon.png">
-                            <span class="card-title black-text">Grafiek</span>
-                            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">insert_chart</i></a>
-                        </div>
-                        <div class="card-content">
-                            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col s12 m6">
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="../../../assets/img/favicon.png">
-                            <span class="card-title black-text">Grafiek</span>
-                            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">insert_chart</i></a>
-                        </div>
-                        <div class="card-content">
-                            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <table class="highlight responsive-table">
-                    <thead>
-                    <tr>
-                        <th>Functie</th>
-                        <th>Niveau</th>
-
-                    </tr>
-                    </thead>
-
-                    <tbody>
-                    <tr>
-                        <td>Winkel</td>
-                        <td>Kiezelsteen</td>
-                    </tr>
-                    <tr>
-                        <td>Horeca</td>
-                        <td>Kei</td>
-                    </tr>
-                    <tr>
-                        <td>Museum</td>
-                        <td>Hunebed</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
     </div>
+
