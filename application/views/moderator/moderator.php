213920027487cd168f29a8fc9edc11b7472fc6f3
Sander111 <sanderpaping@live.nl>
2019-06-19 15:42:50 +0200

    fixed card clicks

--- application/views/moderator/moderator.php
+++ application/views/moderator/moderator.php
@@ -8,56 +8,64 @@
         </section>
         <div class="row">
             <div class="col s12 m3">
-                <div class="card medium hoverable">
-                    <div class="card-image">
-                        <img src="<?= base_url('assets/img/User-Icon.png') ?>">
+                <a href="<?= base_url('moderator/addUser') ?>" class="hb-blue-text">
+                    <div class="card medium hoverable">
+                        <div class="card-image">
+                            <img src="<?= base_url('assets/img/User-Icon.png') ?>">
+                        </div>
+                        <div class="card-content ">
+                            <span class="card-title black-text">Gebruiker toevoegen</span>
+                        </div>
+                        <div class="card-action center">
+                            Voeg een gebruiker toe
+                        </div>
                     </div>
-                    <div class="card-content ">
-                        <span class="card-title black-text">Gebruiker toevoegen</span>
-                    </div>
-                    <div class="card-action center">
-                        <a href="<?= base_url('moderator/addUser') ?>" class="hb-blue-text">Voeg een gebruiker toe</a>
-                    </div>
-                </div>
+                </a>
             </div>
             <div class="col s12 m3">
-                <div class="card medium hoverable">
-                    <div class="card-image">
-                        <img src="<?= base_url('assets/img/vraag.jpg') ?>">
+                <a href="<?= base_url('moderator/addQuestion') ?>" onclick="clearSessionStorage()" class="hb-blue-text">
+                    <div class="card medium hoverable">
+                        <div class="card-image">
+                            <img src="<?= base_url('assets/img/vraag.jpg') ?>">
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title black-text">Vraag toevoegen</span>
+                        </div>
+                        <div class="card-action center">
+                            Voeg een vraag toe
+                        </div>
                     </div>
-                    <div class="card-content">
-                        <span class="card-title black-text">Vraag toevoegen</span>
-                    </div>
-                    <div class="card-action center">
-                        <a href="<?= base_url('moderator/addQuestion') ?>" onclick="clearSessionStorage()" class="hb-blue-text">Voeg een vraag toe</a>
-                    </div>
-                </div>
+                </a>
             </div>
             <div class="col s12 m3">
-                <div class="card medium hoverable">
-                    <div class="card-image">
-                        <img src="<?= base_url('assets/img/statistics.png') ?>">
+                <a href="<?= base_url('moderator/showUsers') ?>" class="hb-blue-text">
+                    <div class="card medium hoverable">
+                        <div class="card-image">
+                            <img src="<?= base_url('assets/img/statistics.png') ?>">
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title black-text">Gebruikers</span>
+                        </div>
+                        <div class="card-action center">
+                            Bekijk Gebruikers
+                        </div>
                     </div>
-                    <div class="card-content">
-                        <span class="card-title black-text">Gebruikers</span>
-                    </div>
-                    <div class="card-action center">
-                        <a href="<?= base_url('moderator/showUsers') ?>" class="hb-blue-text">Bekijk Gebruikers</a>
-                    </div>
-                </div>
+                </a>
             </div>
             <div class="col s12 m3">
-                <div class="card medium hoverable">
-                    <div class="card-image">
-                        <img src="<?= base_url('assets/img/QA.jpg') ?>">
+                <a href="<?= base_url('moderator/allQuestions') ?>" class="hb-blue-text">
+                    <div class="card medium hoverable">
+                        <div class="card-image">
+                            <img src="<?= base_url('assets/img/QA.jpg') ?>">
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title black-text">Alle vragen</span>
+                        </div>
+                        <div class="card-action center">
+                            Alle vragen
+                        </div>
                     </div>
-                    <div class="card-content">
-                        <span class="card-title black-text">Alle vragen</span>
-                    </div>
-                    <div class="card-action center">
-                        <a href="<?= base_url('moderator/allQuestions') ?>" class="hb-blue-text">Alle vragen</a>
-                    </div>
-                </div>
+                </a>
             </div>
         </div>
     </div>
