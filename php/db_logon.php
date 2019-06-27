835b419d97d67e520d58d5a798b947ee1bbe52b7
Soner Mulder <sonermulder@gmail.com>
2019-06-27 13:17:41 +0200

    niet meer nodig

--- php/db_logon.php
+++
@@ -1,6 +0,0 @@
-<?php
-    //This file server to set local DB logon data, to prevent conflicts in usernames and such.
-    //I'm not giving you my db info and don't want yours if it isn't "root", ""
-    $username = "";
-    $password = ""; 
-?>
